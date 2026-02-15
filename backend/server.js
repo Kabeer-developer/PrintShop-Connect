c11eab10b8b1e97c1bab40d2fa95ff6d404194be
kabeer AR <kabeeratwork7@gmail.com>
2026-02-15 18:33:27 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,7 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      process.env.FRONTEND_URL,
-    ],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
